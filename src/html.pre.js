75b3b2f55dd385a9ccc384eb8242ed69ca502889
Conrad Woeltge <conrad.woeltge@w-friends.de>
2018-09-13 11:37:53 +0200

    Update model to sections

--- src/html.pre.js
+++ src/html.pre.js
@@ -15,43 +15,186 @@
  * limitations under the License.
  *
  */
+const select = require('unist-util-select');
+const visit = require('unist-util-visit');
+const toHAST = require('mdast-util-to-hast');
+const toHTML = require('hast-util-to-html');
+const mdastSqueezeParagraphs = require('mdast-squeeze-paragraphs');
+const mdastFlattenImages = require('mdast-flatten-image-paragraphs');
+const mdastFlattenLists = require('mdast-flatten-listitem-paragraphs');
+
+
+/**
+ * The LayoutMachine is an implmentation of a state machine pattern
+ * that tries to intelligently lay out the page.
+ */
+const LayoutMachine = {
+  /*
+    States:
+      init -> hero, flow
+      hero -> flow
+  */
+  validStates: ['hero', 'flow', 'gallery'],
+  states: ['init'],
+  get state() {
+    return this.states[this.states.length - 1];
+  },
+  set state(v) {
+    console.log(`${this.state} -> ${v}`);
+
+    this.states.push(v);
+    return v;
+  },
+  layout: function (section) {
+    // allow manual overide of class
+    // this might be instant-cruft–discuss.
+    if (section.class && section.class.length) {
+      // If class is a valid state, use it, otherwise default to 'flow'
+      if (this.validStates.includes(section.class)) {
+        this.states.push(section.class);
+      } else {
+        this.states.push('flow');
+      }
+    } else {
+      switch (this.state) {
+        case 'init':
+          if (this.isHero(section)) {
+            this.state = 'hero';
+          } else {
+            if (this.isGallery(section)) {
+              this.state = 'gallery';
+            } else {
+              this.state = 'flow';
+            }
+          }
+          break;
+        case 'flow':
+        case 'hero':
+          if (this.isGallery(section)) {
+            this.state = 'gallery';
+          } else {
+            this.state = 'flow';
+          }
+          break;
+      }
+      section.class = this.state;
+    }
+
+    let children = [];
+    for (let e of section.children) {
+      children.push(e);
+    }
+    section.children = children;
+    return section;
+  },
+
+  get hasHero() {
+    return this.states.includes('hero');
+  },
+
+  isHero(section) {
+    // If the section has an h2 & an image in the first level, it's a hero
+    const image = select(section, 'image');
+    const h = select(section, 'heading');
+    return (h.length == 1 && (h[0].depth == 1 || h[0].depth == 2) && image.length == 1);
+  },
+
+  isGallery(section) {
+    // If the section has more than 2 images, it is a gallery
+    const image = select(section, 'image');
+    return image.length > 2
+  },
+}
+
+function getSmartDesign(mdast) {
+  mdast = mdastFlattenImages()(mdast);
+  mdast = mdastFlattenLists()(mdast);
+  mdast = mdastSqueezeParagraphs(mdast);
+
+  const mdastNodes = mdast.children;
+
+  const sections = [];
+  let currentSection = {
+    children: [],
+    type: 'standard'
+  };
+
+  let title;
+
+  mdastNodes.forEach(function (node) {
+    if (node.type == "heading" && node.depth == 1 && !title) {
+      title = node.children[0].value;
+      return;
+    }
+    if (node.type == "thematicBreak") {
+      sections.push(LayoutMachine.layout(currentSection));
+      currentSection = {
+        children: [],
+        type: 'standard'
+      };
+    } else {
+      currentSection.children.push(node);
+    }
+  });
+
+  sections.push(LayoutMachine.layout(currentSection));
+  return sections;
+}
+
+function computeSectionsHAST(sections) {
+  const nodes = [];
+  let odd = false;
+  sections.forEach(function (section) {
+    const hast = toHAST(section);
+    const htmlNodes = [];
+    hast.children.forEach(function (h) {
+      htmlNodes.push(toHTML(h));
+    });
+    nodes.push({
+      type: "element",
+      properties: {
+        className: section.class + ' ' + ((odd = !odd) ? 'odd' : 'even'),
+      },
+      tagName: 'section',
+      children: hast.children
+    });
+  });
+  return nodes;
+}
 
 /**
  * The 'pre' function that is executed before the HTML is rendered
  * @param payload The current payload of processing pipeline
- * @param payload.resource The content resource
+ * @param payload.content The content resource
  */
 function pre(payload) {
-  payload.resource.time = `${new Date()}`;
-  payload.resource.subHeadline = getSubheadline(payload.resource)
-  payload.resource.image = getMainImage(payload.resource)
+
+  // banner is first image and banner text is image alt
+  payload.content.banner = {
+    img: '',
+    text: ''
+  };
+  const firstImg = select(payload.content.mdast, 'image');
+  if (firstImg.length > 0) {
+    payload.content.banner = {
+      img: firstImg[0].url,
+      text: firstImg[0].alt
+    }
+  }
+
+  const sections = getSmartDesign(payload.content.mdast);
+  console.log('sections', sections);
+  const sectionsHAST = computeSectionsHAST(sections);
+  const html = toHTML({
+    type: 'root',
+    children: sectionsHAST
+  })
+  payload.content.sections = html;
+
+  // avoid htl execution error if missing
+  payload.content.meta = payload.content.meta || {};
+  payload.content.meta.references = payload.content.meta.references || [];
+  payload.content.meta.icon = payload.content.meta.icon || '';
 }
-
-function getMainImage(resource) {
-  headlineFound = false;
-  returnValue = "";
-  resource.mdast.children.some(function (child) {
-    if (child.type == 'paragraph' && child.children[0].type == 'image') {
-      returnValue = child.children[0]
-      return true
-    }
-  });
-  return returnValue;
-}
-
-function getSubheadline(resource) {
-  headlineFound = false;
-  returnValue = "";
-  resource.mdast.children.some(function (child) {
-    if (headlineFound && child.type == 'paragraph') {
-      returnValue = child.children[0].value
-      return true
-    }
-
-    if (child.type == 'heading' && child.depth == 1) {
-      headlineFound = true;
-    }
-  });
-  return returnValue;}
 
 module.exports.pre = pre;
