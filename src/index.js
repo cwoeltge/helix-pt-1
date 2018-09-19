67722f92c676cc9a24667120aa9471625f4eacc7
Natalia Venditto <natalia.venditto@netcentric.biz>
2018-09-19 11:01:22 +0200

    add parallax enhancement

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
 import './css/main.scss'
+import { parallaxHero } from './js/parallax/parallaxHero';
 
-/*   document.addEventListener('scroll', () => {
-    const top = window.pageYOffset;
-    const image = document.querySelectorAll('.image__img'); 
-    const heroImage = document.querySelector('.header__parallaxImg');
-    const heroHl = document.querySelector('.hero .panel__base');
-    const heroRect = heroHl.getBoundingClientRect();
-    const heroImgRect = heroImage.getBoundingClientRect();
-
-    // heroImage.style.transform = 'translate3d(0,' + heroImgRect.y * 1.03 + 'px, 0)';
-    // image.style.transform = 'translate3d(0,' + heroImgRect.y * 1.03 * -0.3 + 'px, 0)';
-
-    //heroHl.style.transform = 'scale(' + heroRect.width * 1.03 + ', ' + heroRect.height * 1.03 + ')';
-    image.forEach(function(){
-      let imgRect = image.getBoundingClientRect();
-      image.style.backgroundPosition = imgRect * 0.3 + 'px center';
-    })
-  }); */
-
+parallaxHero();
